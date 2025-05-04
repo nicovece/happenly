00b320c4a17ee3cca305d8b997ab26a6e794955e
Nico Vece <me@nicovece.com>
2025-05-04 19:44:00 +0200

    Added Atatus

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import * as atatus from 'atatus-spa';
+atatus.config('2a57695fcbdb439089e1f7e59f12b0a4').install();
+atatus.notify(new Error('Test Atatus Setup'));
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
